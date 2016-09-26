227b75cb78556749389665e8d24ffb1a856195a2
Cortland <cortland.lizer.15@cnu.edu>
2016-09-26 09:24:44 -0400

    idk stuff

---
+++ Eclipse/270 project one recursion/src/Recursion.java
@@ -0,0 +1,73 @@
+
+public class Recursion {
+    public Recursion() {// default constructor
+        // thro exception UnsupportedOperationException
+    }
+
+    int CAt6 = 5;
+
+    public static int factorial(Integer num) {
+        if (num == 1) {
+            return 1;
+        }
+
+        return num * factorial(num - 1);
+    }
+
+    public static boolean isPalindrome(String string) {
+        string = string.replaceAll("\\s", "");
+        string = string.toUpperCase();
+        // System.out.println(string);
+        if (string.length() < 2) {
+            return true;
+        }
+
+        else {
+            char front = string.charAt(0);
+            char end = string.charAt(string.length() - 1);
+            return (front == end)
+                    && isPalindrome(string.substring(1, string.length() - 1));
+
+        }
+        // return true;
+    }
+
+    public static boolean isIdentifier(String string) {
+        // if (Character.isAlphabetic(string.charAt(0)))
+       // int end = string.charAt(string.length() - 1);
+        if (string.length() == 1 && Character.isLetter(string.charAt(0))) {
+            return true;
+        }
+        else if (isAlphaNum(string.charAt(string.length() - 1)) && string.length()> 1) {
+
+            return isIdentifier(string.substring(0, string.length() - 1));
+
+        }
+
+        return false;
+    }
+
+    private static boolean isAlphaNum(char c) {
+        if (Character.isLetter(c) || Character.isDigit(c) || c == '_') {
+            return true;
+        }
+        return false;
+
+    }
+
+    public static int factor(String string) {
+
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(factorial(5));
+        System.out.println(isPalindrome("taco Cat"));
+        System.out.println(isIdentifier("cat6"));
+        System.out.println(isIdentifier("cat 6"));
+
+        System.out.println(isIdentifier("Cat_6"));
+
+    }
+
+}
---
+++ Eclipse/270 project one recursion/src/RecursionTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecursionTest {
+
+	@Test
+	public void testConstructor() { //UnsupportedOperationException
+		
+		{
+			Recursion test = new Recursion();
+			// actual = a.getSpeed();
+			//int expected = -50;
+			//assertEquals("", expected, actual);
+		}
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testFactor() {
+		
+		{
+			//Car a = new Car(-51);
+			//int actual = a.getSpeed();
+			//int expected = -50;
+			//assertEquals("", expected, actual);
+		}
+		
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testFactorial() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testIsIdentifier() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIsPalindrome() {
+		fail("Not yet implemented");
+	}
+
+}
